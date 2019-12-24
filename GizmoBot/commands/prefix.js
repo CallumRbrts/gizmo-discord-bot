1cc1e0f819f5fa1b74a7cc204744993d4f5bfeec
caroberts <callum.roberts@etu.uca.fr>
2019-12-24 02:02:05 +0100

    added a mysql database (will only work for my laptop) to fix the global prefix change and lay foundations for furture storage + added custom status to bot + changed static command system to a dynamic one

--- GizmoBot/commands/prefix.js
+++ GizmoBot/commands/prefix.js
@@ -3,17 +3,24 @@ const onlySpecialCharsRegex = /[^a-zA-Z0-9]+$/;
 module.exports = {
   name: 'prefix',
   description: 'Change the prefix',
-  execute(message, args, PREFIX) {
+  async execute(message, args, PREFIX, keyvPrefixes) {
       if(!args[1]){
-       message.channel.send('Prefix is currently "' + PREFIX + '"');
+       // message.channel.send('Prefix is currently "' + PREFIX + '"');
+       let get = await keyvPrefixes.get(message.guild.id);
+       if(get){
+         return message.channel.send('Prefix is currently "' + get + '"' );
+       }
+        return message.channel.send('Prefix is currently "' + PREFIX + '"' );
       }else if(!message.member.roles.find(r => r.name === "Moderator")){
          message.channel.send('You need the "Moderator" role to use this command. You will need to create the role or get someone to assign it for you.');
       }else if(!onlySpecialCharsRegex.test(args[1])){
          message.reply('The last character of the prefix should be a special character (!,#,?...)')
       }else{
-        PREFIX = args[1];
-        message.channel.send('Prefix has been changed to "'+ PREFIX+'"');
-        return PREFIX;
+        //PREFIX = args[1];
+        await keyvPrefixes.set(message.guild.id, args[1]);
+        return message.channel.send('Prefix has been changed to "'+args[1]+'"');
+        //message.channel.send('Prefix has been changed to "'+ PREFIX+'"');
+        //return PREFIX;
       }
   }
 }
--- GizmoBot/index.js
+++ GizmoBot/index.js
@@ -4,13 +4,17 @@ bot.commands = new Discord.Collection();
 //const token = 'NjU4MzEyMTkyODk4MTA1MzQ0.Xf-Adw.mUM9kTE9H_QLhb0xTAUU9kfIhMo';
 //var PREFIX = '!';
 const fs = require('fs');
+const Keyv = require('keyv');
+const keyvPrefixes = new Keyv('mysql://hart:Ilovemydog@localhost/gizmo');
 const dir = './images/characters';
+const VERSION = '1.1.0';
 var {PREFIX, token} = require('./config.json');
 var crypto = require('./crypto.js');
 var CHANCE = 1; //0.09 is the optimal
-const VERSION = '1.0.0';
 var NBFILES;
 var FILEDIRS = [];
+
+keyvPrefixes.on('error', err => console.error('Keyv connection error:', err));
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
@@ -31,15 +35,68 @@ bot.login(token);
 
 bot.on('ready', () => {
   console.log('This bot is online!');
+	bot.user.setPresence({
+		status: "online",
+		game: {
+			name: PREFIX + "help",
+			type: "PLAYING"
+		}
+	})
 });
 
-bot.on('message', message=>{
+function imagePop(message){
+	let pop = Math.random();
+	//chance to send a random image of a game character
+	if(pop < CHANCE){
+		//message.channel.send('I am in');
+		//
+	 let imageNumber = Math.floor (Math.random() * (NBFILES - 1 + 1));
+		//ways to hide image name: 1- try embedding the image, 2- use dictionary 3-make it like waifu bot
+		//Next create a data structure with the file dir,
+		let name = FILEDIRS[imageNumber].substring(0, FILEDIRS[imageNumber].indexOf('.'));
+		name = name.charAt(0).toUpperCase() + name.slice(1);
+		let embed = new Discord.RichEmbed()
+			.setColor('#3880ba')
+			.setTitle('New character has been discovered!')
+			.attachFiles([dir+'/'+FILEDIRS[imageNumber]])
+			.setDescription('Type '+PREFIX+'get <character> to guess the \n character\'s name!' )
+			.setImage('attachment://'+FILEDIRS[imageNumber]);
+		message.channel.send(embed);
+		return;
+	}
+}
+
+bot.on('message', async message=>{
   if(message.author.bot){
     return;
   }
-  if (message.content.substring(0, PREFIX.length) == PREFIX) {
+	let args;
 
-    let args = message.content.substring(PREFIX.length).split(" ");
+	if(message.guild){
+		let prefix;
+		if(!await keyvPrefixes.get(message.guild.id)){
+			prefix = PREFIX;
+		} else {
+			const guildPrefix = await keyvPrefixes.get(message.guild.id);
+			if(message.content.startsWith(guildPrefix)){
+				prefix = guildPrefix;
+				PREFIX = prefix;
+			}
+		}
+		//Consider getting rid of this so I don't have a disgusting code duplication
+		// if(!prefix){
+	  //   return imagePop(message);
+		// }
+		if(prefix){
+			args = message.content.substring(prefix.length).split(/\s+/);
+		}
+
+	}else{
+		args = message.content.substring(PREFIX.length).split(/\s+/);
+	}
+
+	if (message.content.substring(0, PREFIX.length) == PREFIX){
+  //args = message.content.substring(PREFIX.length).split(/\s+/);
     let command = args[0].toLowerCase();
 
     //checks collection for the command, if found then proceed
@@ -53,38 +110,16 @@ bot.on('message', message=>{
             chosenCommand.execute(message, args, VERSION);
             break;
           case 'prefix':
-            let newPrefix = chosenCommand.execute(message, args, PREFIX);
-            if(newPrefix){
-              PREFIX = newPrefix;
-            }
+            chosenCommand.execute(message, args, PREFIX, keyvPrefixes);
             break;
           default:
             chosenCommand.execute(message, args);
             break;
         }
+
     }
-  }else{
-    //generates a number between 0 and 1
-    let pop = Math.random();
-    //chance to send a random image of a game character
-    if(pop < CHANCE){
-      //message.channel.send('I am in');
-      //
-     let imageNumber = Math.floor (Math.random() * (NBFILES - 1 + 1));
-      //ways to hide image name: 1- try embedding the image, 2- use dictionary 3-make it like waifu bot
-      //Next create a data structure with the file dir,
-      let name = FILEDIRS[imageNumber].substring(0, FILEDIRS[imageNumber].indexOf('.'));
-      name = name.charAt(0).toUpperCase() + name.slice(1);
-      let embed = new Discord.RichEmbed()
-        .setColor('#3880ba')
-        .setTitle('New character has been discovered!')
-        .attachFiles([dir+'/'+FILEDIRS[imageNumber]])
-        .setDescription('Type '+PREFIX+'get <character> to guess the \n character\'s name!' )
-        .setImage('attachment://'+FILEDIRS[imageNumber]);
-
-
-      message.channel.send(embed);
-    }
-
-  }
+	}else{
+		//need to call this function again so that the images may still spawn even when the server prefix hasn't changed
+		return imagePop(message);
+	}
 });
